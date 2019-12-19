32592c45d36d69da206f3267d1ed50b970821013
rripamonti <rripamonti@studenti.uninsubria.it>
2019-12-19 16:07:44 +0100

    fix

--- src/Test.java
+++ src/Test.java
@@ -21,7 +21,8 @@ public class Test{
             System.out.println("Connected");
             ResultSet resultSet;
             JDBCStatement statement = (JDBCStatement) c.createStatement();
-            resultSet= statement.getExecutionPlan("SELECT MAX(I.ID),AVG(I.CUSTOMERID) FROM INVOICE as I join CUSTOMER as C on I.CUSTOMERID = C.ID WHERE I.ID IN (SELECT INVOICEID FROM ITEM as IT join PRODUCT as P on IT.PRODUCTID = P.ID where COST > (SELECT TOP 1 ID FROM CUSTOMER))");
+            resultSet= statement.executeQuery("EXPLAIN JSON PLAN FOR SELECT * FROM INVOICE as I join CUSTOMER as C on I.CUSTOMERID = C.ID WHERE I.TOTAL=50 AND I.ID IN (SELECT INVOICEID FROM ITEM as IT join PRODUCT as P on IT.PRODUCTID = P.ID where COST > (SELECT TOP 1 ID FROM CUSTOMER))");
+            //resultSet = statement.getExecutionPlan("SELECT MAX(I.ID),AVG(I.TOTAL) FROM INVOICE as I join CUSTOMER as C on I.CUSTOMERID = C.ID WHERE I.TOTAL=50 AND I.ID IN (SELECT INVOICEID FROM ITEM as IT join PRODUCT as P on IT.PRODUCTID = P.ID where COST > (SELECT TOP 1 ID FROM CUSTOMER))");
             while (resultSet.next()){
                 System.out.println(resultSet.getObject(1).toString());
             }
