24b5abb669aedd7c63beca96e11099c417b5143c
rripamonti <rripamonti@studenti.uninsubria.it>
2020-01-25 14:43:39 +0100

    test classes

--- src/Test.java
+++ src/Test.java
@@ -21,7 +21,11 @@ public class Test{
             System.out.println("Connected");
             ResultSet resultSet;
             JDBCStatement statement = (JDBCStatement) c.createStatement();
-            resultSet= statement.executeQuery("EXPLAIN JSON PLAN FOR SELECT * FROM INVOICE as I join CUSTOMER as C on I.CUSTOMERID = C.ID WHERE I.TOTAL=50 AND I.ID IN (SELECT INVOICEID FROM ITEM as IT join PRODUCT as P on IT.PRODUCTID = P.ID where COST > (SELECT TOP 1 ID FROM CUSTOMER))");
+            //resultSet= statement.executeQuery("explain json minimal for SELECT C.ID FROM INVOICE as I join CUSTOMER as C on I.CUSTOMERID = C.ID WHERE I.TOTAL>200");
+            //resultSet= statement.executeQuery("explain json minimal for SELECT I.ID, I.TOTAL FROM INVOICE as I");
+            //resultSet= statement.executeQuery("explain json minimal for SELECT I.ID, I.TOTAL FROM INVOICE as I WHERE I.TOTAL>100");
+            //resultSet= statement.executeQuery("explain json minimal for SELECT I.CUSTOMERID, MAX(I.TOTAL) FROM INVOICE as I group by (I.CUSTOMERID) having I.CustomerID is not null");
+            resultSet= statement.executeQuery("explain json minimal for SELECT AVG(TOTAL) as average,MAX(I.ID) as ID_IN,C.ID FROM INVOICE as I left join CUSTOMER as C on CUSTOMERID = C.ID WHERE TOTAL>200 GROUP BY C.ID having AVG(TOTAL)>200");
             //resultSet = statement.getExecutionPlan("SELECT MAX(I.ID),AVG(I.TOTAL) FROM INVOICE as I join CUSTOMER as C on I.CUSTOMERID = C.ID WHERE I.TOTAL=50 AND I.ID IN (SELECT INVOICEID FROM ITEM as IT join PRODUCT as P on IT.PRODUCTID = P.ID where COST > (SELECT TOP 1 ID FROM CUSTOMER))");
             while (resultSet.next()){
                 System.out.println(resultSet.getObject(1).toString());
